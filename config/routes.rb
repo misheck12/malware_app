f885bccd7fd32d6b253400d21c979e0f589bd245
misheck12 <misheck1720@gmail.com>
2023-08-14 09:41:44 +0000

    Aug 14, 2023, 9:41 AM

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
